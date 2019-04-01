a9b76a1d448e321c2403597f83e8350edab4814f
Pavel Marek <pavel.akira.marek@gmail.com>
2019-04-01 15:50:51 +0200

    Remove set CXX_COMPILER from cmake

---
+++ levenstein/my_main.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 3.13)
+project(my_main)
+
+#set(CMAKE_CXX_COMPILER clang++-7)
+#add_compile_options("-Wall" "-msse4.2")
+#set(CMAKE_CXX_STANDARD 17)
+
+#set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g")
+#set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
+
+add_executable(my_main
+        my_main.cpp
+        du4levenstein.hpp
+        dummy_levenstein.hpp)
