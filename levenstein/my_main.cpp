3e22186829d4845b3a601bc831bdf52e9716acba
Pavel Marek <pavel.akira.marek@gmail.com>
2019-04-05 14:57:31 +0200

    First run SSE vector tests, then AVX512 vector tests, then functional

--- levenstein/my_main.cpp
+++ levenstein/my_main.cpp
@@ -29,26 +29,49 @@ static void run_levenstein()
 
 static void run_all_avx512_tests()
 {
+    std::cout << "=================================" << std::endl;
+    std::cout << "Running tests specific for AVX512" << std::endl;
+    std::cout << "=================================" << std::endl;
+
     LevensteinTester<policy_avx512> tester;
-    FunctionalTester<policy_avx512> functional_tester;
     tester.run_all_tests();
-    functional_tester.run_all_tests();
 }
 #endif // USE_AVX512
 
 
 static void run_all_sse_tests()
 {
+    std::cout << "==============================" << std::endl;
+    std::cout << "Running tests specific for SSE" << std::endl;
+    std::cout << "==============================" << std::endl;
+
     LevensteinTester<policy_sse> tester;
-    FunctionalTester<policy_sse> functional_tester;
     tester.run_all_tests();
+}
+
+/// For both SSE and AVX512
+static void run_functional_tests()
+{
+    std::cout << "===========================================" << std::endl;
+    std::cout << "Running functional tests for SSE and AVX512" << std::endl;
+    std::cout << "===========================================" << std::endl;
+
+    FunctionalTester<policy_sse> functional_tester;
     functional_tester.run_all_tests();
 }
 
 int main()
 {
+    auto a1 = {23,25,13,15,18,20,23,25,13,18,25,11,12,17,20,23,24};
+    auto a2 = {30,30,30,15,18,20,30,30,30,30,30,30,30,30,20,23};
+    assert(a2.size() == 16);
+    assert(a1.size() == 17);
+    dummy_levenstein dummy{a1.begin(), a1.end(), a2.begin(), a2.end()};
+    dummy.compute();
+
     run_all_sse_tests();
 #ifdef USE_AVX512
     run_all_avx512_tests();
 #endif
+    run_functional_tests();
 }
